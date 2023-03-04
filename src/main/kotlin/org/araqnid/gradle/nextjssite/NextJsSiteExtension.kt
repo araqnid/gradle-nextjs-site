442ee69d7c872633d57a85dfcc04a55cc1dfd07e
Steve Haslam <araqnid@gmail.com>
2023-03-04 19:39:03 +0000

    Move @Suppress up to class

--- src/main/kotlin/org/araqnid/gradle/nextjssite/NextJsSiteExtension.kt
+++ src/main/kotlin/org/araqnid/gradle/nextjssite/NextJsSiteExtension.kt
@@ -2,17 +2,15 @@ package org.araqnid.gradle.nextjssite
 
 import org.gradle.api.provider.Property
 
+@Suppress("LeakingThis")
 abstract class NextJsSiteExtension {
     abstract val debugBuild: Property<Boolean>
     abstract val productionProfiling: Property<Boolean>
     abstract val lint: Property<Boolean>
 
     init {
-        @Suppress("LeakingThis")
         debugBuild.convention(false)
-        @Suppress("LeakingThis")
         productionProfiling.convention(false)
-        @Suppress("LeakingThis")
         lint.convention(true)
     }
 }
